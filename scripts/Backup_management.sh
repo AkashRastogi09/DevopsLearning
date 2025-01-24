61dacb3812c29419dd6ca13c31c19255a4e3e983
Akash Rastogi <akashrastogi889@gmail.com>
2025-01-24 17:49:54 +0530

    Update Backup_management.sh

--- scripts/Backup_management.sh
+++ scripts/Backup_management.sh
@@ -1,32 +1,47 @@
 #!/bin/bash
 
-
-<<Note
-
-This is script is basicly use for Bash backup Diretory.
-We will take inpur from User to backup the Diretory.
-
-
+: <<'Note'
+This script is used for backing up a directory in Bash.
+The user provides input to specify the source directory to back up and the destination directory.
 Note
 
-#this is Source Directory path.
+# Source Directory path
 SRC_DIR=$1
 
-#this is Destination Directory Path.
+# Destination Directory path
 DEST_DIR=$2
 
-#this is the Time stamp.
+# Timestamp for the backup
 Time_stamp=$(date +"%m-%d-%y-%H-%M-%S")
 
-# this is the File Name with Time stamp.
+# Backup file name with timestamp
 Backup_file="$DEST_DIR/Backup_$Time_stamp.tar"
 
-echo "The File Backup begin provcess"
+echo "The backup process begins..."
 
-tar -cf $Backup_file $SRC_DIR
+# Create the backup file
+tar -cf "$Backup_file" "$SRC_DIR"
 
+# Check if the backup was successful
 if [ $? -eq 0 ]; then
-        echo "Backup process has been Successfull in $SRC_SIR Location"
+    echo "Backup process was successful."
 else
-        echo "Backup process has been failed"
+    echo "Backup process failed."
 fi
+
+: <<'Note'
+This module manages backup rotation.
+It retains only the latest 5 backup files and deletes older ones.
+Note
+
+# Destination directory path for rotation
+DESK_DIR="$DEST_DIR"
+
+# List all files in the directory, sorted by modification time (newest first)
+files=( $(ls -1t "$DESK_DIR") )
+
+# Loop through the files and delete files exceeding the 5 most recent
+for ((i=5; i<${#files[@]}; i++)); do
+    echo "Deleting file: ${files[i]}"
+    rm -f "$DESK_DIR/${files[i]}"
+done
